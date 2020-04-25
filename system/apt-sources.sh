ef45e067526fea6cbb96992de993db3276d24e84
Jesse Stricker <me@jessestricker.de>
2020-04-26 00:10:15 +0200

    Add root check, Improve style

--- system/apt-sources.sh
+++ system/apt-sources.sh
@@ -1,25 +1,35 @@
-#!/bin/bash
+#!/bin/sh
 
 set -eu
 
-# example:
-# deb http://archive.ubuntu.com/ubuntu focal main restricted universe multiverse
+# ensure root user
+if [ "$(id -u)" -ne 0 ]; then
+	echo "error: this script must be run as root"
+	exit 1
+fi
 
-uri="http://archive.ubuntu.com/ubuntu"
+# define constants and path
+repo_url="http://archive.ubuntu.com/ubuntu"
 distro=$(lsb_release --codename --short)
-suites="$distro ${distro}-updates ${distro}-security ${distro}-backports"
+suites="${distro} ${distro}-updates ${distro}-security ${distro}-backports"
 comps="main restricted universe multiverse"
+
 file="/etc/apt/sources.list"
 backup_file="${file}.bak-$(date --utc +%Y%m%d-%H%M%S)"
 
 # do backup
-mv "$file" "$backup_file"
+echo "backing up ${file} -> ${backup_file}"
+mv "${file}" "${backup_file}"
 
 # echo into file
-for suite in $suites; do
-	line="deb $uri $suite $comps"
-	echo "$line" >> "$file"
+for suite in ${suites}; do
+	line="deb ${repo_url} ${suite} ${comps}"
+	echo "${line}" >> "${file}"
 done
+
+# display
+echo "the new content of ${file}:"
+cat "${file}"
 
 # clean and update
 sudo apt-get clean
